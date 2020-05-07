879823d2837065d90c3055e07a388f2bbe10c517
Gumplen-kish <gumplen117@gmail.com>
2020-05-07 07:10:47 +0700

    Добавлен спрайт и т.д

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,29 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var svgstore = require('gulp-svgstore');
+var rename = require("gulp-rename");
+var svgmin = require('gulp-svgmin');
+var path = require('path');
+
+gulp.task('svgstore', function () {
+    return gulp.src('source/img/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(rename("sprite.svg"))
+        .pipe(gulp.dest('source/img'));
+});
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
